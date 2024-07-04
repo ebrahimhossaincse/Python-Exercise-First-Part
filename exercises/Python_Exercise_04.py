c2d0dedf4fe7f0286bf1e46e0e884bddf257f882
ebrahimhossaincse <mdebrahimhossain.me@gmail.com>
2024-07-04 14:03:15 -0400

    python exercise 04

---
+++ exercises/Python_Exercise_04.py
@@ -0,0 +1,6 @@
+'''
+Write a program to find the index of 7 in the list numbers=[3, 5, 1, 9, 7, 2, 8].
+'''
+numbers = [3, 5, 1, 9, 7, 2, 8]
+index_of_7 = numbers.index(7)
+print("The index of 7 is:", index_of_7)
