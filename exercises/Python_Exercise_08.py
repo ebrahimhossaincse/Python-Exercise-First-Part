c352e0a5a3f048660b9dd665fdbfc18188eeb877
ebrahimhossaincse <mdebrahimhossain.me@gmail.com>
2024-07-04 20:06:55 -0400

    python exercise 08

---
+++ exercises/Python_Exercise_08.py
@@ -0,0 +1,21 @@
+'''
+Write a function that takes a number 1 to 9 from the user input (use input function inside a function).
+Store a number in a variable (let’s assume 6).
+If the input value is less than the variable, print "Your guess is almost there".
+If the input value is greater than the variable, print "Your guess is higher".
+If the input value and variable are equal, print "Your Guess Is Correct!".
+'''
+
+def guessing_game():
+    stored_number = 5
+    guess = int(input("Guess a number between 1 and 9: "))
+
+    if guess < stored_number:
+        print("Your guess is almost there")
+    elif guess > stored_number:
+        print("Your guess is higher")
+    else:
+        print("Your Guess Is Correct!")
+
+# Example usage
+guessing_game()
